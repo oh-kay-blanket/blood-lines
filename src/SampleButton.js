19f05ca419e1e44607399955366890f1eedf1cec
Michael Plunkett <mplunkett@sallybeauty.com>
2023-02-13 11:42:03 -0600

    More styling

--- src/SampleButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SampleButton = ({ name, loadFile }) => {
-  return(
-    <input className='sampleButton' type='button' value={name} onClick={loadFile} />
-  )
-}
-
-export default SampleButton
